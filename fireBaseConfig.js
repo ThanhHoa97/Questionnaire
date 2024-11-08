d07bcc207c00495f8779c3cf201a4b1419e03612
Thi Thanh Hoa TRAN <tthithanhhoa@gmail.com>
2024-11-08 14:55:59 +0100

    Add FireBaseConfig

---
+++ fireBaseConfig.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB3YF0LJmMG_xO6JzO4GC8gX3rzLQHeM8M",
+    authDomain: "questionnaire-956d0.firebaseapp.com",
+    projectId: "questionnaire-956d0",
+    storageBucket: "questionnaire-956d0.firebasestorage.app",
+    messagingSenderId: "984927383017",
+    appId: "1:984927383017:web:64593b1c1f379c6dfc2f78",
+    measurementId: "G-3P3EZQGE89"
+};
+
+
+
+
+
